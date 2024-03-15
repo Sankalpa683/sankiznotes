707480fbb0b1c33f0812158f0fd4a5a19c404d0c
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-03-15 19:15:12 +0545

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,23 @@ import { siteConfig } from '@/config/siteConfig';
 const website = () => {
   return (
     <div>
-      <Head><title>{siteConfig.productName} - About Me</title>
+      <Head><title>{siteConfig.productName} - About Me</title
+            <meta name="description" content="MozeeeNotes helps 12th Grade Student to Download & study FREE, high-quality 12th Grade Notes for ALL NEB subjects in Nepal! Ace your NEB 12th Grade exams with MozeeeNotes. Master key concepts, prepare for exams, and excel with Mozeeenotes. Get comprehensive study materials, notes, and resources tailored for students in Nepal at mozeee.com" />
+        <meta name="keywords" content="NEB, Nepal, 12th Grade, Study Notes, Exam Preparation, MozeeeNotes" />
+        <meta property="og:title" content="MozeeeNotes - Comprehensive Study Notes for NEB 12th Grade Students in Nepal" />
+        <meta property="og:description" content="MozeeeNotes helps 12th Grade Student to Download & study FREE, high-quality 12th Grade Notes for ALL NEB subjects in Nepal! Ace your NEB 12th Grade exams with MozeeeNotes. Master key concepts, prepare for exams, and excel with Mozeeenotes. Get comprehensive study materials, notes, and resources tailored for students in Nepal at mozeee.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://mozeee.com" />
+        <meta property="og:image" content="https://mozeee.com/logo.png" />
+        <meta property="og:site_name" content="MozeeeNotes" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="MozeeeNotes - Comprehensive Study Notes for NEB 12th Grade Students in Nepal" />
+        <meta name="twitter:description" content="MozeeeNotes helps 12th Grade Student to Download & study FREE, high-quality 12th Grade Notes for ALL NEB subjects in Nepal! Ace your NEB 12th Grade exams with MozeeeNotes. Master key concepts, prepare for exams, and excel with Mozeeenotes. Get comprehensive study materials, notes, and resources tailored for students in Nepal at mozeee.com" />
+        <meta name="twitter:image" content="https://mozeee.com/logo.png" />
+        
+
+  <link rel='icon' href='/ico.png'></link>
       </Head>
       <Nav/><br></br><br></br><br></br>
       <section class="flex items-center bg-stone-100 xl:h-screen font-poppins dark:bg-gray-800 ">
