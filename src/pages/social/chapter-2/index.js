16b989ff137d6467bb74f5f8940df96b7e21ce4c
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-03-31 14:58:47 +0545

    Update index.js

--- src/pages/social/chapter-2/index.js
+++ src/pages/social/chapter-2/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import Nav from '../../../nav.js';
-import Footer from '../../../footer.js'
+import Nav from '../../nav.js'
+import Footer from '../../footer.js'
 import Head from 'next/head.js'
-import styles from '../../chapters.module.css';
+import styles from '../chapters.module.css';
 
 function helloworld() {
   return (
     <div>
         <Head>
-            <title>Economics - Unit-2 Digital and General Research Skills as Useful Life Skills (जीवनपयोगी सिपको रुपमा डिजिटल तथा सामान्य अनुसन्धान) सिप Class 12 Social Studies and Life Skills Notes | MozeeeNotes</title>
+            <title>Unit-2 Digital and General Research Skills as Useful Life Skills (जीवनपयोगी सिपको रुपमा डिजिटल तथा सामान्य अनुसन्धान) सिप Class 12 Social Studies and Life Skills Notes | MozeeeNotes</title>
         </Head>
         <Nav/>
         <br></br><br></br><br></br>
@@ -18,6 +18,9 @@ function helloworld() {
             <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
               <li class="inline-flex items-center">
                 <a href="/" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                  {/* <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
+                  </svg> */}
                   Home
                 </a>
               </li>
@@ -26,7 +29,7 @@ function helloworld() {
                   <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                   </svg>
-                  <a href="/social" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Economics</a>
+                  <a href="/computer" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Computer</a>
                 </div>
               </li>
               <li aria-current="page">
@@ -42,7 +45,7 @@ function helloworld() {
         
             </div><br></br>
             <div className='heaading_text'>
-                <h1 class={styles.heading}>Unit-2 Digital and General Research Skills as Useful Life Skills (जीवनपयोगी सिपको रुपमा डिजिटल तथा सामान्य अनुसन्धान) सिप Class 12 Social Studies and Life Skills Notes</h1>
+                <h1 class={styles.heading}>ObjectUnit-2 Digital and General Research Skills as Useful Life Skills (जीवनपयोगी सिपको रुपमा डिजिटल तथा सामान्य अनुसन्धान) सिप Class 12 Social Studies and Life Skills Notes</h1>
             </div>
             <div className={styles.upload_time}>
             <iframe src="https://cdn.gurubaa.com/wp-content/uploads/2023/04/11095752/Unit-2-Digital-and-General-Research-Skills-as-Useful-Life-Skills-.pdf" width="100%" height="780" allow="autoplay"></iframe>
