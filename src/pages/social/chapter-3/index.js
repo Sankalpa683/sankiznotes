19650762e5aa29e489cc3f619acc3bd6e62788bd
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-03-31 15:08:40 +0545

    Update index.js

--- src/pages/social/chapter-3/index.js
+++ src/pages/social/chapter-3/index.js
@@ -8,7 +8,7 @@ function helloworld() {
   return (
     <div>
         <Head>
-            <title>Unit -1 Interrelationship of Social Studies and Life long Learning Class 12 Social Studies and Life Skills Notes | MozeeeNotes</title>
+            <title>Unit-3 Life Useful Skill  Notes | MozeeeNotes</title>
         </Head>
         <Nav/>
         <br></br><br></br><br></br>
@@ -18,9 +18,6 @@ function helloworld() {
             <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
               <li class="inline-flex items-center">
                 <a href="/" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
-                  {/* <svg class="w-3 h-3 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
-                  </svg> */}
                   Home
                 </a>
               </li>
@@ -37,7 +34,7 @@ function helloworld() {
                   <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                   </svg>
-                  <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">सामाजिक-अध्ययन-र-जीवन-शिक्षाको-अन्तरसम्बन्ध</span>
+                  <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">जीवन-उपयोगी-सिप</span>
                 </div>
               </li>
             </ol>
@@ -45,10 +42,10 @@ function helloworld() {
         
             </div><br></br>
             <div className='heaading_text'>
-                <h1 class={styles.heading}>Chapter-1 सामाजिक अध्ययन र जीवन शिक्षाको अन्तरसम्बन्ध - Social Studies </h1>
+                <h1 class={styles.heading}>Chapter-3 जीवन उपयोगी सिप - Social Studies </h1>
             </div>
             <div className={styles.upload_time}>
-            <iframe src="https://cdn.gurubaa.com/wp-content/uploads/2023/04/11092028/Unit-1-Interrelationship-of-Social-Studies-and-Life-long-Learning.pdf" width="100%" height="780" allow="autoplay"></iframe>
+            <iframe src="https://cdn.gurubaa.com/wp-content/uploads/2023/04/11120531/Unit-3-Life-Useful-Skills-pdf.pdf" width="100%" height="780" allow="autoplay"></iframe>
             </div>
         </div><br></br><br></br>
         <div>
