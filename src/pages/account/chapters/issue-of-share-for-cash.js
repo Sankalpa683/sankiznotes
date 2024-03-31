c95a5239d28db3fc919b33e56a0bae6d70e721af
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-03-31 21:31:50 +0545

    Update issue-of-share-for-cash.js

--- src/pages/account/chapters/issue-of-share-for-cash.js
+++ src/pages/account/chapters/issue-of-share-for-cash.js
@@ -31,7 +31,7 @@ function issue_cash() {
                   <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                   </svg>
-                  <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">company-formation</span>
+                  <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">issue-of-share-for-cash</span>
                 </div>
               </li>
             </ol>
