77c272b251a17456e3321331e524b71e2e1b4a51
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-03-31 21:32:10 +0545

    Update forfeiture-and-re-issue-of-shares.js

--- src/pages/account/chapters/forfeiture-and-re-issue-of-shares.js
+++ src/pages/account/chapters/forfeiture-and-re-issue-of-shares.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 function forfeiture() {
   return (
+    <br></br><br></br><br></br>
     <div class='container mx-auto px-5 py-5'>
         <iframe src="https://drive.google.com/file/d/1eiahr1yDbnqEms-vNw-AJCrRXcs2mN1h/preview" width="100%" height="780" allow="autoplay"></iframe>
     <br></br><br></br><br></br>
