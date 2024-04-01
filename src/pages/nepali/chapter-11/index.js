57dd437b5504ee259a07bc187fcd85d0ece3abb7
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-01 18:42:18 +0545

    Create index.js

---
+++ src/pages/nepali/chapter-11/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import Nav from '../../nav.js'
+import Footer from '../../footer.js'
+import Head from 'next/head.js'
+import styles from '../chapters.module.css';
+
+function helloworld() {
+  return (
+    <div>
+        <Head>
+            <title>Unit-11 सहकारी  | MozeeeNotes</title>
+        </Head>
+        <Nav/>
+        <br></br><br></br><br></br>
+        <div class="container mx-auto px-5 py-5">
+            <div className={styles.chapter_breadcrumbs}>
+          <nav class="flex" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+              <li class="inline-flex items-center">
+                <a href="/" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                  Home
+                </a>
+              </li>
+              <li>
+                <div class="flex items-center">
+                  <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                  </svg>
+                  <a href="/nepali" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Nepali</a>
+                </div>
+              </li>
+              <li aria-current="page">
+                <div class="flex items-center">
+                  <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                  </svg>
+                  <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">सहकारी</span>
+                </div>
+              </li>
+            </ol>
+          </nav>
+        
+            </div><br></br>
+            <div className='heaading_text'>
+                <h1 class={styles.heading}>Chapter-11 सहकारी  - Nepali </h1>
+            </div>
+            <div className={styles.upload_time}>
+            <iframe src="https://drive.google.com/file/d/1iDIm2H-zC2QSzrTv4EmIR3r_u73q48qO/preview" width="100%" height="780" allow="autoplay"></iframe>
+            </div>
+        </div><br></br><br></br>
+        <div>
+
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default helloworld
