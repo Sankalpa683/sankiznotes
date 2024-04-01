403f37b3dd7d8f637367751bc479bc91c77c0117
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-01 19:51:52 +0545

    Update index.js

--- src/pages/nepali/chapter-3/index.js
+++ src/pages/nepali/chapter-3/index.js
@@ -45,7 +45,7 @@ function helloworld() {
                 <h1 class={styles.heading}>Chapter-3 घनघस्याको उकालो काट्ता  - Nepali </h1>
             </div>
             <div className={styles.upload_time}>
-            <iframe src="https://drive.google.com/file/d/1iDIm2H-zC2QSzrTv4EmIR3r_u73q48qO/preview" width="100%" height="780" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1YGpqfrXMcwyXv1PCi9UwlD8ysYG-8fl2/preview" width="100%" height="780" allow="autoplay"></iframe>
             </div>
         </div><br></br><br></br>
         <div>
