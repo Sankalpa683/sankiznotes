c92dc9e3d6fe4534656b4762f14772e91f3ed642
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-01 21:53:56 +0545

    Update index.js

--- src/pages/nepali/chapter-6/index.js
+++ src/pages/nepali/chapter-6/index.js
@@ -45,7 +45,7 @@ function helloworld() {
                 <h1 class={styles.heading}>Chapter-6 स्टिफन विलियम हकिङ  - Nepali </h1>
             </div>
             <div className={styles.upload_time}>
-            <iframe src="https://drive.google.com/file/d/1iDIm2H-zC2QSzrTv4EmIR3r_u73q48qO/preview" width="100%" height="780" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1XF9_h_9WcPBmU0nwy5U5MHdkSz13Y--3/preview" width="100%" height="780" allow="autoplay"></iframe>
             </div>
         </div><br></br><br></br>
         <div>
