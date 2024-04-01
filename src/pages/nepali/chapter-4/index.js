789383b7c7ab92c40351ae56be1e696be7efb72f
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-01 19:52:27 +0545

    Update index.js

--- src/pages/nepali/chapter-4/index.js
+++ src/pages/nepali/chapter-4/index.js
@@ -45,7 +45,7 @@ function helloworld() {
                 <h1 class={styles.heading}>Chapter-4 व्यावसायिक पत्र  - Nepali </h1>
             </div>
             <div className={styles.upload_time}>
-            <iframe src="https://drive.google.com/file/d/1iDIm2H-zC2QSzrTv4EmIR3r_u73q48qO/preview" width="100%" height="780" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1jbx5doHfZ0ZXRkeyg7uj_BXvJ1JZE2_p/preview" width="100%" height="780" allow="autoplay"></iframe>
             </div>
         </div><br></br><br></br>
         <div>
