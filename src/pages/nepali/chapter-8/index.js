8a510934cad9ee9801d66c9b0d5944a1f90b8784
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-01 21:52:13 +0545

    Update index.js

--- src/pages/nepali/chapter-8/index.js
+++ src/pages/nepali/chapter-8/index.js
@@ -45,7 +45,7 @@ function helloworld() {
                 <h1 class={styles.heading}>Chapter-8 मातृत्व  - Nepali </h1>
             </div>
             <div className={styles.upload_time}>
-            <iframe src="https://drive.google.com/file/d/1iDIm2H-zC2QSzrTv4EmIR3r_u73q48qO/preview" width="100%" height="780" allow="autoplay"></iframe>
+            <iframe src="https://drive.google.com/file/d/1R0szriOE8q1U3SIsaV2Sjn8IT2rxHQa7/preview" width="100%" height="780" allow="autoplay"></iframe>
             </div>
         </div><br></br><br></br>
         <div>
