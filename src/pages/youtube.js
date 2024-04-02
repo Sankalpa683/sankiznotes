7ffe621626d1d1ebc4df16d53014816df44953ec
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-02 11:32:52 +0545

    Update youtube.js

--- src/pages/youtube.js
+++ src/pages/youtube.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import YoutubeSearch from '../components/YoutubeSearch';
+import YoutubeSearch from '../component/YoutubeSearch';
 
 export default function Home() {
   return (
