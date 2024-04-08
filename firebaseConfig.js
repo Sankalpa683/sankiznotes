5f8781d900411cf4a33c6865d4c7cbc89f7409cf
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-08 10:05:40 +0545

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHI6AQh4Ltujvmkf2VTo33QYmif5O6V2g",
+  authDomain: "alien-craft-362813.firebaseapp.com",
+  projectId: "alien-craft-362813",
+  storageBucket: "alien-craft-362813.appspot.com",
+  messagingSenderId: "1018792046218",
+  appId: "1:1018792046218:web:c58613e344727a2e448773",
+  measurementId: "G-V5RHTVVE7T"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
