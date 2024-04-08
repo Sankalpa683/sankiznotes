7533fc95de23b3e0223b6af3ac1ccc09bea26555
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-08 10:41:57 +0545

    Update notification.js

--- src/pages/notification.js
+++ src/pages/notification.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Head from 'next/head.js'
 import Nav from './nav.js'
 import styles from './notification.module.css'
@@ -9,6 +9,19 @@ import Footer from './footer.js'
 import { siteConfig } from '@/config/siteConfig.js';
 
 function notification() {
+  const [daysLeft, setDaysLeft] = useState(0);
+
+  useEffect(() => {
+    const calculateDaysLeft = () => {
+      const currentDate = new Date();
+      const examDate = new Date('2024-04-26');
+      const differenceInTime = examDate.getTime() - currentDate.getTime();
+      const differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
+      setDaysLeft(differenceInDays);
+    };
+
+    calculateDaysLeft();
+  }, []);
   return (
     <div className={styles.html}>
         <Head>
@@ -26,7 +39,7 @@ function notification() {
                         <img src='/me.jpg'></img>
                     </div>
                     <div className={styles.notification_div_text}>
-                       <p><b>Class 12 </b>Exam is within 11 days. Check The calander here. </p> 
+                       <p><b>Class 12 </b>Exam is within {daysLeft <= 7 ? daysLeft + ' days' : 'a week'}. Check The calendar here</p> 
                     </div>
                     <div className={styles.notification_div_app}>
                         <div className={styles.notification_div_color}></div>
