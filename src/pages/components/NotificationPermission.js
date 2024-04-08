735ffd5c8cc4782dbb7cda9d0d9030223d472db1
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-08 10:19:00 +0545

    Update NotificationPermission.js

--- src/pages/components/NotificationPermission.js
+++ src/pages/components/NotificationPermission.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import firebase from '../../firebaseConfig.js';
+import firebase from '../../../firebaseConfig.js';
 
 const NotificationPermission = () => {
   useEffect(() => {
