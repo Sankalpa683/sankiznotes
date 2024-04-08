6446a93499df68c836ae916766d23d869b565db7
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-08 16:47:07 +0545

    Update computer.js

--- src/pages/model/computer.js
+++ src/pages/model/computer.js
@@ -24,7 +24,7 @@ function helloworld() {
           <nav class="flex" aria-label="Breadcrumb">
             <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
               <li class="inline-flex items-center">
-                <a href="/model" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                <a href="/" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
                   Home
                 </a>
               </li>
@@ -33,7 +33,7 @@ function helloworld() {
                   <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                   </svg>
-                  <a href="/" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">model-question</a>
+                  <a href="/model" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">model-question</a>
                 </div>
               </li>
               <li aria-current="page">
