cd3649c78aae1b9005c314326b0ad88b10cd099b
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-08 14:22:27 +0545

    Create index.js

---
+++ src/pages/model-question/english/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import Nav from '../../nav.js'
+import Footer from '../../footer.js'
+import Head from 'next/head.js'
+import styles from '../chapters.module.css';
+
+function helloworld() {
+  return (
+    <div>
+        <Head>
+            <title>English - NEB Grade 12 Model Question Paper 2079 | MozeeeNotes</title>
+          <meta name="title" content="English NEB Grade 12 Model Question Paper 2079 2023, NEB Class 12  Question Answer 2079 2023 | Mozeee.com">
+          <link href="https://mozeee.com/model-question/english" rel="canonical">
+          <meta name="robots" content="index, follow">
+          <meta property="og:url" content="https://mozeee.com/model-question/english">
+        <meta property="og:image" content="https://mozeee.com/logo.png">
+          <meta name="description" content="Here you will get clear and concise chapter-wise NEB Class 12 Model Questions to enhance your conceptual clarity, prepare for examinations, and achieve academic excellence.">
+          <meta property="og:title" content="English - NEB Grade 12 Model Question Paper 2079 | MozeeeNotes">
+          <meta name="og:description" content="Here you will get clear and concise chapter-wise NEB Class 12 Model Questions to enhance your conceptual clarity, prepare for examinations, and achieve academic excellence.">
+          <meta name="keywords" content="NEB, Class 12 Model Questions, NEB class 12 model questions, model questions, Grade 12 model questions, class 12 notes, class 12 notes nepal, neb notes, model questions nepal, class 12 model questions nepal">
+      
+    </Head>
+        <Nav/>
+        <br></br><br></br><br></br>
+        <div class="container mx-auto px-5 py-5">
+            <div className={styles.chapter_breadcrumbs}>
+          <nav class="flex" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+              <li class="inline-flex items-center">
+                <a href="/" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                  Home
+                </a>
+              </li>
+              <li>
+                <div class="flex items-center">
+                  <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                  </svg>
+                  <a href="/model-question" class="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">model-question</a>
+                </div>
+              </li>
+              <li aria-current="page">
+                <div class="flex items-center">
+                  <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+                  </svg>
+                  <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">English</span>
+                </div>
+              </li>
+            </ol>
+          </nav>
+        
+            </div><br></br>
+            <div className='heaading_text'>
+                <h1 class={styles.heading}>English - NEB Grade 12 Model Question Paper 2079</h1>
+            </div>
+            <div className={styles.upload_time}>
+            <iframe src="https://drive.google.com/file/d/168L6wHcUmLKPSDArJMYn8rRsmuW9tHxT/preview" width="100%" height="780" allow="autoplay"></iframe>
+            </div>
+        </div><br></br><br></br>
+        <div>
+
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default helloworld
