4ef510bde7c8853ae11b9d1da980ccd38d58660f
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-08 10:11:36 +0545

    Create NotificationPermission.js

---
+++ src/pages/components/NotificationPermission.js
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import firebase from '../../../firebaseConfig';
+
+const NotificationPermission = () => {
+  useEffect(() => {
+    const requestNotificationPermission = async () => {
+      const messaging = firebase.messaging();
+      try {
+        await Notification.requestPermission();
+        const token = await messaging.getToken();
+        console.log('Notification permission granted.');
+        console.log('Token:', token);
+        // Send the token to your server for sending notifications
+      } catch (error) {
+        console.error('Error requesting notification permission:', error);
+      }
+    };
+
+    if ('Notification' in window && 'serviceWorker' in navigator && 'PushManager' in window) {
+      requestNotificationPermission();
+    } else {
+      console.log('Push messaging is not supported');
+    }
+  }, []);
+
+  return null;
+};
+
+export default NotificationPermission;
