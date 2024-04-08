34ab92e74b2329f7caba38bb8c840662de5116f9
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-08 10:36:22 +0545

    Update notification.js

--- src/pages/notification.js
+++ src/pages/notification.js
@@ -7,6 +7,26 @@ import Script from 'next/script';
 import Image from 'next/image';
 import Footer from './footer.js'
 import { siteConfig } from '@/config/siteConfig.js';
+import { useEffect, useState } from 'react';
+
+const CalendarPage = () => {
+  const [daysLeft, setDaysLeft] = useState(0);
+
+  useEffect(() => {
+    const calculateDaysLeft = () => {
+      const currentDate = new Date();
+      const examDate = new Date('2024-04-26');
+      const differenceInTime = examDate.getTime() - currentDate.getTime();
+      const differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
+      setDaysLeft(differenceInDays);
+    };
+
+    calculateDaysLeft();
+
+    const interval = setInterval(calculateDaysLeft, 1000 * 60 * 60 * 24);
+
+    return () => clearInterval(interval);
+  }, []);
 
 function notification() {
   return (
@@ -26,7 +46,7 @@ function notification() {
                         <img src='/me.jpg'></img>
                     </div>
                     <div className={styles.notification_div_text}>
-                       <p><b>Class 12 </b>Exam is within a week. Check The calander here. </p> 
+                       <p><b>Class 12 </b>Exam is within {daysLeft} days. Check The calander here. </p> 
                     </div>
                     <div className={styles.notification_div_app}>
                         <div className={styles.notification_div_color}></div>
