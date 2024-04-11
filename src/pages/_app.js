3eb2237e85d83aa27bda48f7095498bdf9bd0fe6
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-04-11 21:57:36 +0545

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,8 +4,22 @@ import Footer from './footer.js'
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start();
+  });
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done();
+  });
+
+  Router.events.on('routeChangeError', () => {
+    NProgress.done();
+  });
+  
   return <> 
   <Head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
     <title>MozeeNotes - Comprehensive Study Notes for NEB 12th Grade Students in Nepal | MozeeeNotes </title>
     <meta name="google-site-verification" content="hgKlR-gh1kqW6e6_8xl2kSzCsHtRMiGGrqAlQAHl1w4" />
             <meta name="description" content="MozeeeNotes helps 12th Grade Student to Download & study FREE, high-quality 12th Grade Notes for ALL NEB subjects in Nepal! Ace your NEB 12th Grade exams with MozeeeNotes. Master key concepts, prepare for exams, and excel with Mozeeenotes. Get comprehensive study materials, notes, and resources tailored for students in Nepal at mozeee.com" />
