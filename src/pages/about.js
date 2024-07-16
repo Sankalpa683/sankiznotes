9d09df5aa88cc31ee3e316f1ad5fbe0f95dc3648
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-07-16 17:32:53 +0545

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -39,9 +39,9 @@ const website = () => {
                 <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
                     <div class="relative lg:max-w-md">
                         <img src="/sambhav.jpeg" alt="sambhav"
-                            class="relative z-10 object-cover w-full rounded"></img>
+                            class="relative z-10 object-cover w-full rounded"></img><br/>
                         <div
-                            class="absolute bottom-0 right-0 z-10 p-8 bg-white border-4 border-blue-500 rounded shadow dark:border-blue-400 lg:-mb-8 lg:-mr-11 sm:p-8 dark:text-gray-300 dark:bg-gray-800 ">
+                            class="bottom-0 right-0 z-10 p-8 bg-white border-4 border-blue-500 rounded shadow dark:border-blue-400 lg:-mb-8 lg:-mr-11 sm:p-8 dark:text-gray-300 dark:bg-gray-800 ">
                             <p class="text-lg font-semibold md:w-72">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                                     class="absolute top-0 left-0 w-16 h-16 text-blue-700 dark:text-gray-300 opacity-10"
