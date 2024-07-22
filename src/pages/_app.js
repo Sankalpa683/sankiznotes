205e0c88ae1776c688e27a5d56637062f333f7f8
Sankalpa Dahal Makarov <62802864+Sankalpa683@users.noreply.github.com>
2024-07-22 17:47:02 +0545

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -21,26 +21,26 @@ Router.events.on('routeChangeError', () => {
 export default function App({ Component, pageProps }) {  
   return <> 
   <Head>
-    <title>MozeeNotes - Comprehensive Study Notes for NEB 12th Grade Students in Nepal | MozeeeNotes </title>
+    <title>Informal Study - Comprehensive Study Notes for NEB 12th Grade Students in Nepal | Informal Study </title>
     <meta name="google-adsense-account" content="ca-pub-6643840488848439" />
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6643840488848439"
      crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.js" integrity="sha512-/CzcPLOqUndTJKlWJ+PkvFh2ETRtkrnxwmULr9LsUU+cFLl7TAOR5gwwD8DRLvtM4h5ke/GQknlqQbWuT9BKdA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <meta name="google-site-verification" content="hgKlR-gh1kqW6e6_8xl2kSzCsHtRMiGGrqAlQAHl1w4" />
-            <meta name="description" content="MozeeeNotes helps 12th Grade Student to Download & study FREE, high-quality 12th Grade Notes for ALL NEB subjects in Nepal! Ace your NEB 12th Grade exams with MozeeeNotes. Master key concepts, prepare for exams, and excel with Mozeeenotes. Get comprehensive study materials, notes, and resources tailored for students in Nepal at mozeee.com" />
-        <meta name="keywords" content="NEB, Nepal, 12th Grade, Study Notes, Exam Preparation, MozeeeNotes" />
-        <meta property="og:title" content="MozeeeNotes - Comprehensive Study Notes for NEB 12th Grade Students in Nepal" />
-        <meta property="og:description" content="MozeeeNotes helps 12th Grade Student to Download & study FREE, high-quality 12th Grade Notes for ALL NEB subjects in Nepal! Ace your NEB 12th Grade exams with MozeeeNotes. Master key concepts, prepare for exams, and excel with Mozeeenotes. Get comprehensive study materials, notes, and resources tailored for students in Nepal at mozeee.com" />
+            <meta name="description" content="informalstudy.info helps 12th Grade Student to Download & study FREE, high-quality 12th Grade Notes for ALL NEB subjects in Nepal! Ace your NEB 12th Grade exams with Informal Study. Master key concepts, prepare for exams, and excel with Informal Study. Get comprehensive study materials, notes, and resources tailored for students in Nepal at informalstudy.info" />
+        <meta name="keywords" content="NEB, Nepal, 12th Grade, Study Notes, Exam Preparation, Informal Study" />
+        <meta property="og:title" content="Informal Study - Comprehensive Study Notes for NEB 12th Grade Students in Nepal" />
+        <meta property="og:description" content="Informal Study helps 12th Grade Student to Download & study FREE, high-quality 12th Grade Notes for ALL NEB subjects in Nepal! Ace your NEB 12th Grade exams with Informal Study. Master key concepts, prepare for exams, and excel with Informal Study. Get comprehensive study materials, notes, and resources tailored for students in Nepal at informalstudy.info" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://mozeee.com" />
-        <meta property="og:image" content="https://mozeee.com/logo.png" />
-        <meta property="og:site_name" content="MozeeeNotes" />
+        <meta property="og:url" content="https://informalstudy.info" />
+        <meta property="og:image" content="https://informalstudy.info/logo.png" />
+        <meta property="og:site_name" content="Informal Study" />
         <meta property="og:locale" content="en_US" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="MozeeeNotes - Comprehensive Study Notes for NEB 12th Grade Students in Nepal" />
-        <meta name="twitter:description" content="MozeeeNotes helps 12th Grade Student to Download & study FREE, high-quality 12th Grade Notes for ALL NEB subjects in Nepal! Ace your NEB 12th Grade exams with MozeeeNotes. Master key concepts, prepare for exams, and excel with Mozeeenotes. Get comprehensive study materials, notes, and resources tailored for students in Nepal at mozeee.com" />
-        <meta name="twitter:image" content="https://mozeee.com/logo.png" />
+        <meta name="twitter:title" content="Informal Study - Comprehensive Study Notes for NEB 12th Grade Students in Nepal" />
+        <meta name="twitter:description" content="Informal Study helps 12th Grade Student to Download & study FREE, high-quality 12th Grade Notes for ALL NEB subjects in Nepal! Ace your NEB 12th Grade exams with Informal Study. Master key concepts, prepare for exams, and excel with Informal Study. Get comprehensive study materials, notes, and resources tailored for students in Nepal at informalstudy.info" />
+        <meta name="twitter:image" content="https://informalstudy.info/logo.png" />
         
 
   <link rel='icon' href='/ico.png'></link>
@@ -48,6 +48,6 @@ export default function App({ Component, pageProps }) {
   <Nav/>
   <Component {...pageProps} /> 
   <Footer/>
-    <script defer data-domain="mozeee.com" src="https://poopup.co/js/script.js"></script>
+    <script defer data-domain="informalstudy.info" src="https://poopup.co/js/script.js"></script>
   </>
 }
